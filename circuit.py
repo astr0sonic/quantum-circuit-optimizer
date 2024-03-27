bfb3c677fa6d7aacfd260f4b10e333890b4b5c59
astrosonic <z.astrosonic@gmail.com>
2024-03-27 14:07:27 +0300

    add circuit construction

---
+++ circuit.py
@@ -0,0 +1,52 @@
+from qiskit import QuantumCircuit
+from sympy.combinatorics import GrayCode
+
+from utils import get_diff_index
+
+
+def get_quantum_circuit(
+    target: int, num_qubits: int, params: list[float], path_matrix: list[list[list[int]]]
+) -> QuantumCircuit:
+    """Construct a for quantum hashing/fingerprinting adopted for the topology represented by the matrix
+    of the shortest paths between all pairs of qubits
+
+    Args:
+        target (int): index of the target qubit
+        num_qubits (int): _number of qubits
+        params (list[float]): list of parameters of the scheme
+        path_matrix (list[list[list[int]]]): matrix of the shortest paths between all pairs of qubits
+
+    Returns:
+        QuantumCircuit: adopted quantum circuit
+    """
+    qc = QuantumCircuit(num_qubits)
+    apply_hadamard(qc)
+
+    current_code = GrayCode(num_qubits - 1)
+    for i in range(0, 2 ** (num_qubits - 1)):
+        qc.ry(params[i], target)
+        qc.barrier()
+
+        control = get_diff_index(current_code, target, num_qubits)
+        path = path_matrix[control][target]
+        rev_path = list(reversed(path))
+
+        for i in range(len(path) - 1):
+            qc.cx(path[i], path[i + 1])
+        for i in range(1, len(rev_path) - 1):
+            qc.cx(rev_path[i + 1], rev_path[i])
+        qc.barrier()
+
+        current_code = current_code.next()
+
+    return qc
+
+
+def apply_hadamard(qc: QuantumCircuit) -> None:
+    """Apply Hadamard gate for each qubit in the circuit
+
+    Args:
+        qc (QuantumCircuit): quantum circuit
+    """
+    for i in range(qc.num_qubits):
+        qc.h(i)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from graph import floyd_warshall, get_matrix_from_edges
+from circuit import get_quantum_circuit
+from graph import floyd_warshall, get_matrix_from_edges, get_shortest_paths
 from optimizer import optimize_cnots
 
 if __name__ == "__main__":
@@ -31,8 +32,45 @@ if __name__ == "__main__":
         (8, 9),
     ]
 
+    # num_qubits = 27
+    # edges = [
+    #     (0, 1),
+    #     (1, 4),
+    #     (4, 7),
+    #     (7, 10),
+    #     (10, 12),
+    #     (12, 15),
+    #     (15, 18),
+    #     (18, 21),
+    #     (21, 23),
+    #     (6, 7),
+    #     (17, 18),
+    #     (1, 2),
+    #     (2, 3),
+    #     (12, 13),
+    #     (13, 14),
+    #     (23, 24),
+    #     (24, 25),
+    #     (3, 5),
+    #     (5, 8),
+    #     (8, 11),
+    #     (11, 14),
+    #     (14, 16),
+    #     (16, 19),
+    #     (19, 22),
+    #     (22, 25),
+    #     (25, 26),
+    #     (8, 9),
+    #     (19, 20),
+    # ]
+
     adj_matrix = get_matrix_from_edges(num_qubits, edges)
-    dist_matrix, path_matrix = floyd_warshall(adj_matrix)
-    optimal_indices, cnots_count = optimize_cnots(num_qubits, dist_matrix)
-    print(optimal_indices)
-    print(cnots_count)
+    dist_matrix, aux_path_matrix = floyd_warshall(adj_matrix)
+    optimal_indices, cnots_counts = optimize_cnots(num_qubits, dist_matrix)
+    print(f"Optimal indices: {optimal_indices}")
+    print(f"CNOTs counts: {cnots_counts}")
+
+    target = optimal_indices[0]
+    params = [1.0] * (2**num_qubits)
+    path_matrix = get_shortest_paths(aux_path_matrix)
+    qc = get_quantum_circuit(target, num_qubits, params, path_matrix)
